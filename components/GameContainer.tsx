f331d25dde001575879db84315b51d17e3320284
emeiziying <dzq1007650105@gmail.com>
2024-06-16 23:39:23 +0800

    ✨ feat(game):

--- components/GameContainer.tsx
+++ components/GameContainer.tsx
@@ -5,20 +5,14 @@ import ItemEntity from '@/components/ItemEntity'
 import { useMountedState } from '@/hooks/useMountedState'
 import { Language, type Entities } from '@/models'
 import { useAppDispatch, useAppSelector } from '@/store/hooks'
-import {
-  getDataRecord,
-  loadMod,
-  type DatasetPayload,
-} from '@/store/modules/datasetsSlice'
+import { loadMod, type DatasetPayload } from '@/store/modules/datasetsSlice'
 import { SET_LANGUAGE } from '@/store/modules/preferencesSlice'
 import { getAdjustedDataset } from '@/store/modules/recipesSlice'
 import {
   SET_RESEARCHED_TECHNOLOGIES,
   getAvailableRecipes,
-  getResearchedTechnologyIds,
 } from '@/store/modules/settingsSlice'
 import { Card, CardBody, Tab, Tabs } from '@nextui-org/react'
-import { useWhyDidYouUpdate } from 'ahooks'
 import { useTranslations } from 'next-intl'
 import { useEffect, useMemo } from 'react'
 
@@ -27,38 +21,8 @@ const GameContainer = ({ modData }: { modData: DatasetPayload }) => {
   const mounted = useMountedState()
   const t = useTranslations()
 
-  const dataRecord = useAppSelector(getDataRecord)
   const adjustedDataset = useAppSelector(getAdjustedDataset)
   const availableRecipes = useAppSelector(getAvailableRecipes)
-  const researchedTechnologyIds = useAppSelector(getResearchedTechnologyIds)
-
-  const technology = useMemo(() => {
-    let selection: string[] = researchedTechnologyIds || []
-    const set = new Set(selection)
-    const available: string[] = []
-    const locked: string[] = []
-
-    let technologyIds = adjustedDataset.technologyIds
-
-    const researched = selection
-
-    for (const id of technologyIds) {
-      if (!set.has(id)) {
-        const tech = adjustedDataset.technologyEntities[id]
-
-        if (
-          tech.prerequisites == null ||
-          tech.prerequisites.every((p) => set.has(p))
-        ) {
-          available.push(id)
-        } else {
-          locked.push(id)
-        }
-      }
-    }
-
-    return { available, locked, researched }
-  }, [adjustedDataset, researchedTechnologyIds])
 
   const categoryRows = useMemo(() => {
     const allIdsSet = new Set(availableRecipes)
@@ -91,16 +55,6 @@ const GameContainer = ({ modData }: { modData: DatasetPayload }) => {
     dispatch(loadMod(modData))
     dispatch(SET_RESEARCHED_TECHNOLOGIES([]))
   }, [dispatch, modData])
-
-  useWhyDidYouUpdate('Home', [
-    dataRecord,
-    adjustedDataset,
-    technology,
-    availableRecipes,
-    categoryIds,
-    categoryRows,
-    researchedTechnologyIds,
-  ])
 
   if (!mounted) return null
 
