2fde8ed2c9624d953ee80cf0c2271c10153d9445
emeiziying <dzq1007650105@gmail.com>
2024-06-28 00:12:50 +0800

    ✨ feat(item):

--- src/components/ItemEntity.tsx
+++ src/components/ItemEntity.tsx
@@ -3,6 +3,7 @@ import IconItem from '@/components/IconItem';
 import { useAppDispatch, useAppSelector } from '@/store/hooks';
 import { getAdjustedDataset } from '@/store/modules/recipesSlice';
 import { recordsState, type ItemRecord } from '@/store/modules/recordsSlice';
+import { Tooltip } from '@mui/material';
 import { useMemo } from 'react';
 import { useTranslation } from 'react-i18next';
 
@@ -37,42 +38,57 @@ const ItemEntity = ({ id }: Props) => {
 
   return (
     <div>
-      <div className="p-1">
-        <IconItem name={itemEntity.icon ?? id} text={itemEntity.iconText} />
-        <div>
-          {itemEntity.name}-{itemEntity.id}
-          <div>time:{recipeEntity.time.toNumber()}</div>
-          <div>stock:{itemRecord?.stock.toNumber() ?? 0}</div>
-          {canManualCrafting && <CraftingButton id={id} />}
-        </div>
-      </div>
+      <Tooltip
+        open
+        title={
+          <div className="text-black">
+            <div>
+              {itemEntity.name}({t('data.recipe')})
+            </div>
+            <div>
+              <div>{t('data.ingredients')}:</div>
+              <div>
+                {Object.keys(recipeEntity.in).map((id) => (
+                  <div key={id} className="flex items-center">
+                    <IconItem name={id}></IconItem>
+                    <div>
+                      {recipeEntity.in[id].toNumber()} x{' '}
+                      {adjustedDataset.recipeEntities[id]?.name}
+                    </div>
+                  </div>
+                ))}
+              </div>
+              <div className="flex justify-end items-center">
+                {recipeEntity.time.toNumber()}s time
+              </div>
+            </div>
+            <div className="flex items-start">
+              <div>
+                <div>原料合计:</div>
+                <div>total time</div>
+              </div>
+              <div>{/*  */}</div>
+            </div>
 
-      <div className="text-black">
-        <div className="flex items-center">
-          <div>time:{recipeEntity.time.toNumber()}</div>
-          <div className="flex">
-            {Object.keys(recipeEntity.in).map((id) => (
-              <IconItem key={id} name={id}>
-                {recipeEntity.in[id].toNumber()}
-              </IconItem>
-            ))}
+            <div className="flex items-center">
+              <div className="pr-1">{t('data.producers')}:</div>
+              {recipeEntity.producers.map((id) => (
+                <IconItem key={id} name={id} />
+              ))}
+            </div>
           </div>
-          {'->'}
-          <div className="flex">
-            {Object.keys(recipeEntity.out).map((id) => (
-              <IconItem key={id} name={id}>
-                {recipeEntity.out[id].toNumber()}
-              </IconItem>
-            ))}
+        }
+      >
+        <div className="p-1">
+          <IconItem name={itemEntity.icon ?? id} text={itemEntity.iconText} />
+          <div>
+            {itemEntity.name}-{itemEntity.id}
+            <div>time:{recipeEntity.time.toNumber()}</div>
+            <div>stock:{itemRecord?.stock.toNumber() ?? 0}</div>
+            {canManualCrafting && <CraftingButton id={id} />}
           </div>
         </div>
-        <div className="flex items-center">
-          <div className="pr-1">{t('data.producers')}:</div>
-          {recipeEntity.producers.map((id) => (
-            <IconItem key={id} name={id} />
-          ))}
-        </div>
-      </div>
+      </Tooltip>
     </div>
   );
 };
