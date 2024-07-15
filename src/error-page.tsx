0062dcdb3c95b0c18bea90aa2a28992eb83571f3
emeiziying <dzq1007650105@gmail.com>
2024-07-15 16:27:33 +0800

    ✨ feat(error): add clear button while error

--- src/error-page.tsx
+++ src/error-page.tsx
@@ -1,3 +1,4 @@
+import Button from '@mui/material/Button';
 import { useRouteError } from 'react-router-dom';
 
 export default function ErrorPage() {
@@ -6,13 +7,22 @@ export default function ErrorPage() {
   return (
     <div
       id="error-page"
-      className="p-10 text-center justify-center flex flex-col h-full w-full absolute"
+      className="absolute flex h-full w-full flex-col justify-center p-10 text-center"
     >
       <h1>Oops!</h1>
       <p>Sorry, an unexpected error has occurred.</p>
       <p>
         <i>{error.statusText || error.message}</i>
       </p>
+
+      <Button
+        onClick={() => {
+          localStorage.clear();
+          window.location.reload();
+        }}
+      >
+        Clear Storeage
+      </Button>
     </div>
   );
 }
